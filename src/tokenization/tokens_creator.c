c4c95d3d24728f8a76542cd2a98f3bda582d6be2
Celia Codere <ccodere@c1r4p6.42quebec.com>
2024-12-19 14:24:27 -0500

    removed comments in token creator

--- src/tokenization/tokens_creator.c
+++ src/tokenization/tokens_creator.c
@@ -6,7 +6,7 @@
 /*   By: ccodere <ccodere@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:07:11 by ccodere           #+#    #+#             */
-/*   Updated: 2024/12/19 13:47:14 by ccodere          ###   ########.fr       */
+/*   Updated: 2024/12/19 14:22:26 by ccodere          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,54 +65,23 @@ char	**transformer(t_minishell *ms)
 	char	**tmp;
 	int		initial_size;
 
-	// ft_fprintf(2, "pretokens:\n");
-	// print_debug(ms->pretokens);
 	parser(ms, ms->pretokens);
 	initial_size = count_tokens(ms->pretokens);
 	init_int_arrays(ms, ms->pretokens);
-	// ft_fprintf(2, "token.quoted:\n");
-	// print_int_array(ms->pretokens, &ms->token.quoted);
-	// ft_fprintf(2, "token.expanded:\n");
-	// print_expanded_array(ms->pretokens, &ms->token.expanded);
 	ms->expanded = expander(ms, ms->pretokens);
 	free_tokens_address(&ms->pretokens);
 	if (!ms->expanded)
 		return (NULL);
-	// ft_fprintf(2, "expander:\n");
-	// print_debug(ms->expanded);
-	// ft_fprintf(2, "After expander:\n");
-	// print_state_array(ms, ms->expanded, count_tokens(ms->expanded));
 	tmp = trimmer(ms, ms->expanded);
 	free_tokens_address(&ms->expanded);
-	// ft_fprintf(2, "trimmer:\n");
-	// print_debug(tmp);
-	// print_state_array(ms, tmp, count_tokens(tmp));
-	// ft_fprintf(2, "token.quoted:\n");
-	// print_int_array(tmp, &ms->token.quoted);
-	// ft_fprintf(2, "token.expanded:\n");
-	// print_expanded_array(tmp, &ms->token.expanded);
-	// ft_fprintf(2, "After update state array:\n");
-	// print_state_array(ms, tmp, count_tokens(tmp));
 	final_tokens = retokenizer(ms, tmp, initial_size);
 	free_tokens_address(&tmp);
-	// ft_fprintf(2, "retokenizer:\n");
-	// print_debug(final_tokens);
-	// ft_fprintf(2, "token.quoted:\n");
-	// print_int_array(final_tokens, &ms->token.quoted);
-	// ft_fprintf(2, "token.expanded:\n");
-	// print_expanded_array(final_tokens, &ms->token.expanded);
 	if (final_tokens && !has_type(final_tokens, &ms->token.quoted,
 			&ms->token.expanded, is_pipe))
 	{
 		tmp = cleaner(ms, final_tokens);
 		free_tokens_address(&final_tokens);
 		final_tokens = tmp;
-		// ft_fprintf(2, "cleaner:\n");
-		// print_debug(final_tokens);
-		// ft_fprintf(2, "token.quoted:\n");
-		// print_int_array(final_tokens, &ms->token.quoted);
-		// ft_fprintf(2, "token.expanded:\n");
-		// print_expanded_array(final_tokens, &ms->token.expanded);
 	}
 	free_state_array(ms, initial_size);
 	return (final_tokens);
